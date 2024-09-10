cadc9d5d4f5ea0fa5ed2de641f75f740671bece6
gacc94 <gustavocaqui94@gmail.com>
2024-09-09 23:56:17 -0500

    config styles

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,8 +1,28 @@
 import { Routes } from "@angular/router";
+import { LayoutComponent } from "./presentation/views/layout/layout.component";
 
 const AUTH_ROUTES: Routes = [
   {
-    path: ''
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'sign-in',
+        loadComponent: () => import('./presentation/views/sign-in/sign-in.component').then(m => m.SignInComponent)
+      },
+      {
+        path: '**',
+        redirectTo: 'sign-in',
+        pathMatch: 'full'
+      }
+      // { path: 'about', loadChildren: () => import('./presentation/views/about/about.module').then(m => m.AboutModule) },
+      // { path: 'contact', loadChildren: () => import('./presentation/views/contact/contact.module').then(m => m
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
   }
 ]
 
