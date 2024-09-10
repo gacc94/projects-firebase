2f5f1e79dacdb2ec80625162a16cb7b49820c856
gacc94 <gustavocaqui94@gmail.com>
2024-09-10 01:59:53 -0500

    add styles sign in

--- src/app/features/auth/presentation/views/sign-in/sign-in.component.ts
+++ src/app/features/auth/presentation/views/sign-in/sign-in.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, DestroyRef } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { ActivatedRoute, Router, RouterLink } from "@angular/router";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { Auth } from '@app/utils/libraries/app-constant';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    RouterLink,
+    MatSnackBarModule
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export default class SignInComponent {
 
+  authForm!: FormGroup;
+
+  constructor(
+    private readonly activateRoute: ActivatedRoute,
+    private readonly router: Router,
+    private readonly fb: FormBuilder,
+    private readonly destroyRef: DestroyRef,
+  ) {
+    this.initForm();
+  }
+
+  initForm() {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    })
+  }
+
+  signInGoogle(): void {
+
+  }
 }
